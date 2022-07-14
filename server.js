387014e4b9724a5e5545fa1fbf5285887c35cc18
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-07-14 09:21:23 +0200

    authentication works

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { JobSource } from "./models/JobSource.js";
 import mongoose from "mongoose";
 import cors from "cors";
+import jwt from "jsonwebtoken";
 
 const user = {
     id: 1,
@@ -31,13 +32,42 @@ app.use(express.json());
 
 const port = process.env.PORT || 3044;
 
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers["authorization"];
+    if (typeof bearerHeader !== "undefined") {
+        const bearer = bearerHeader.split(" ");
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+const decodeJwt = (token) => {
+    let base64Url = token.split(".")[1];
+    let base64 = base64Url.replace("-", "+").replace("_", "/");
+    let decodedData = JSON.parse(
+        Buffer.from(base64, "base64").toString("binary")
+    );
+    return decodedData;
+};
+
 app.get("/", (req, res) => {
     res.send("<h1>Book Site API</h1>");
 });
 
-app.get("/job-sources", async (req, res) => {
-    const jobSources = await JobSource.find();
-    res.status(200).json(jobSources);
+app.post("/maintain-login", verifyToken, (req, res) => {
+    jwt.verify(req.token, "secretkey", (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const data = decodeJwt(req.token);
+            res.json({
+                user: data.user,
+            });
+        }
+    });
 });
 
 app.post("/login", (req, res) => {
@@ -46,12 +76,19 @@ app.post("/login", (req, res) => {
     const password = req.body.password;
 
     if (username === "hans" && password === "123") {
-        res.json({
-            user,
+        jwt.sign({ user }, "secretkey", { expiresIn: "20s" }, (err, token) => {
+            res.json({
+                user,
+                token,
+            });
         });
     } else {
         res.sendStatus(403);
     }
+});
+app.get("/job-sources", async (req, res) => {
+    const jobSources = await JobSource.find();
+    res.status(200).json(jobSources);
 });
 
 app.listen(port, () => {
